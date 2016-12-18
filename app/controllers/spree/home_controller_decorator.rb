bd1bede74c72bf7469d7ad07780c45da82ab27de
martin <mrosso10@gmail.com>
2016-12-18 15:34:10 -0300

    Saco el default scope de product y pongo el order en HomeController

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,5 +1,12 @@
 module Spree
   HomeController.class_eval do
+    def index
+      @searcher = build_searcher(params.merge(include_images: true))
+      @products = @searcher.retrieve_products.order("spree_products.position ASC")
+      @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+    end
+
     def contact
     	@message=Message.new
     end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,7 +1,5 @@
 module Spree
   Product.class_eval do
-      default_scope { order("spree_products.position ASC") }
-
       acts_as_list
 
       def all_parents
