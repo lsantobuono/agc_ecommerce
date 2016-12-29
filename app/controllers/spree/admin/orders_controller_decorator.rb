d5297b622191a8df08394263601a4ba73a112026
lsantobuono <luciano.santobuono@gmail.com>
2016-12-29 18:34:17 +0100

    Custom mail info

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -7,5 +7,14 @@ module Spree::Admin
       flash[:success] = Spree.t(:order_approved)
       redirect_to :back
     end
+
+    def resend
+      params[:custom_mail_header] =" HOluu"
+      Spree::OrderMailer.confirm_email(@order.id, true,params[:custom_mail_header]).deliver_later
+      flash[:success] = Spree.t(:order_email_resent)
+
+      redirect_to :back
+    end
+
   end
 end
--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -8,5 +8,14 @@ module Spree
 			mail(to: @order.email, from: from_address, subject: subject)
 		end
 
+    def confirm_email(order, resend = false, custom_text= "")
+      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+      @custom_text = custom_text
+
+      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+      subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
+      mail(to: @order.email, from: from_address, subject: subject)
+    end
+
 	end
 end
