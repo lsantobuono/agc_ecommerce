e602c006ded55b7ac0abb8ddbfaf926e8e890d88
lsantobuono <luciano.santobuono@gmail.com>
2017-01-03 12:25:00 +0100

    varias correcciones lucas

---
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -0,0 +1,10 @@
+module Spree
+  UserMailer.class_eval do
+
+    def registration_notice(user)
+      @name=user.first_name
+      mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' - ' + I18n.t(:subject, :scope => [:devise, :mailer, :registration_notice])
+    end
+
+  end
+end
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -3,8 +3,10 @@ module Spree
 		validates :first_name, :last_name, :phone_number, presence: true
 
 		#Esto evite que devise envie mail de confirmacion, la unica forma de confirmacion es la manual del admin
+    #Y le envia nuestro mail custom de avisarle su user y pass
 		def send_on_create_confirmation_instructions
-        end
+      self.devise_mailer.registration_notice(self).deliver
+    end
 
   end
 end
