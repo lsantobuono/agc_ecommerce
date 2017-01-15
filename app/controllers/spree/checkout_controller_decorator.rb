fc7b007bf17beb7a577e603d2505093d354c700f
Martin <mrosso10@gmail.com>
2017-01-15 15:09:03 -0300

    Bug fix consumidor final

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -5,5 +5,17 @@ module Spree
       # @order.bill_address ||= Address.build_default
       @order.ship_address ||= Address.build_default
     end
+
+    def before_address
+      # if the user has a default address, a callback takes care of setting
+      # that; but if he doesn't, we need to build an empty one here
+      @order.bill_address ||= Address.build_default if should_have_bill_address?
+      @order.ship_address ||= Address.build_default if @order.checkout_steps.include?('delivery')
+    end
+
+    def should_have_bill_address?
+      return true unless params[:order].present? && params[:order][:tipo_factura].present?
+      @order.total > 1000 || params[:order][:tipo_factura] != "consumidor_final"
+    end
   end
 end
