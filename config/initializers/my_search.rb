4210f4618dd190cb2e7e6523ab6c9f9ffa269f67
Martin <mrosso10@gmail.com>
2017-01-24 02:09:36 -0300

    Acentos en la busqueda, fixeado

--- config/initializers/my_search.rb
+++ config/initializers/my_search.rb
@@ -1,11 +1,9 @@
-
 module Spree
   class MySearch < Spree::Core::Search::Base   
     def get_products_conditions_for(base_scope, query)
       unless query.blank?
-
         fields = ["unaccent(spree_products.name)", "spree_products.description", "spree_variants.sku"]
-        values = query.split
+        values = I18n.transliterate(query).split
         where_str = fields.map { |field| Array.new(values.size, "#{field} ILIKE ?").join(' OR ') }.join(' OR ')
 
         base_scope = base_scope.includes(:variants_including_master)
