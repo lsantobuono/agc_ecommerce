1cc46082d835686acec801c422bbf1b25943bdd8
Martin <mrosso10@gmail.com>
2017-01-26 16:41:59 -0300

    Quantifier fix

--- app/models/spree/stock/quantifier_decorator.rb
+++ app/models/spree/stock/quantifier_decorator.rb
@@ -2,17 +2,10 @@ module Spree
   module Stock
     Quantifier.class_eval do
       def can_supply?(required = 1)
-      	auxRequired = variant.security_stock
-      	if (auxRequired == nil)
-      		auxRequired = variant.product.security_stock
-      	end
-      	
-      	if (auxRequired == nil)
-      		auxRequired = required
-		end
-      	# Si la variant tiene security stock, lo uso, si no uso el del producto, si ninguno lo tiene setaeado funciono normalmente con el 
-      	# valor pasado o el 1 default.
-        variant.available? && (total_on_hand >= auxRequired || backorderable?)
+        security_stock = variant.security_stock || variant.product.security_stock || 0
+        # Si la variant tiene security stock, lo uso, si no uso el del producto, si ninguno lo tiene setaeado funciono normalmente con el 
+        # valor pasado o el 1 default.
+        variant.available? && ((total_on_hand - security_stock) >= required || backorderable?)
       end
     end
   end
