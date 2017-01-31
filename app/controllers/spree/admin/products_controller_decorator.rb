cbfa2412151223ffd0e6fead0eef5e7eac61cf1e
Martin <mrosso10@gmail.com>
2017-01-31 18:35:07 -0300

    Fix bug raro

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,21 +1,26 @@
 module Spree::Admin
   ProductsController.class_eval do
-    create.after :create_after
-
-    def location_after_save
-      if params[:add_to_order].present?
-        return :back
-      else
-        spree.edit_admin_product_url(@product)
+    unless respond_to? :el_metodo_que_no_hace_nada # Esto es para fixear un bug raro, por algun motivo carga dos veces este decorator
+      def el_metodo_que_no_hace_nada
       end
-    end
 
-    def create_after
-      if params[:add_to_order].present? && @order = Spree::Order.friendly.find(params[:add_to_order])
-        @order.contents.add(@product.master, 1)
-        @order.next
-        @product.deleted_at = Time.zone.now
-        @product.save
+      create.after :create_after
+
+      def location_after_save
+        if params[:add_to_order].present?
+          return :back
+        else
+          spree.edit_admin_product_url(@product)
+        end
+      end
+
+      def create_after
+        if params[:add_to_order].present? && @order = Spree::Order.friendly.find(params[:add_to_order])
+          @order.contents.add(@product.master, 1)
+          @order.next
+          @product.deleted_at = Time.zone.now
+          @product.save
+        end
       end
     end
   end
