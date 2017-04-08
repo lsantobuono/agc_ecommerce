01fb8d54f54b87450a741e1418b0ce33b7f20cb0
Martin <mrosso10@gmail.com>
2017-04-07 21:06:23 -0300

    Busco los pdfs en la carpeta que corresponda segun el tipo de factura

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -35,10 +35,22 @@ module Spree
 
       def find_pdf_order (number)
         pdf_file_paths = []
-        Find.find('tmp/order_prints') do |path| # Busco en order prints los pdf que correspondan al numero, en un caso normal solo habria 1
+        Find.find("tmp/order_prints/#{pdf_folder}") do |path| # Busco en order prints los pdf que correspondan al numero, en un caso normal solo habria 1
           pdf_file_paths << path if path =~ /.*#{number}\.pdf$/
         end
         pdf_file_paths.last # Si la orden fue cambiada por el admin va a haber mas de 1 pdf, y como siempre se ordenan x id creciente, sacar el ultimo me da el mas reciente.
+      end
+
+      def pdf_folder
+        if (@order.tipo_factura == "consumidor_final" )
+          'invoice_cfs'
+        elsif (@order.tipo_factura == "factura_a")
+          'invoice_as'
+        elsif (@order.tipo_factura == "factura_b")
+          'invoice_bs'
+        else
+          'invoice_mostradors'
+        end
       end
 	end
 end
