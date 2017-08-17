fe8dc9bb90578d7f5f96f7a469ffc7d866925257
lsantobuono <luciano.santobuono@gmail.com>
2017-08-17 13:07:50 +0200

    Busqueda en combos

--- app/controllers/spree/admin/combos_controller.rb
+++ app/controllers/spree/admin/combos_controller.rb
@@ -19,11 +19,28 @@ module Spree::Admin
 
     def collection
       return @collection if @collection.present?
-      params[:q] ||= {}
-      params[:q][:deleted_at_null] ||= "1"
+      @collection = super
 
-      params[:q][:s] ||= "name asc"
-      @collection = Combo.where(nil)
+      if request.xhr? && params[:q].present?
+        @collection = @collection.includes(:category)
+                          .where("combos.name #{LIKE} :search
+                                  OR combos.code #{LIKE} :search 
+                                  OR combos.hidden = :search
+                                  OR (categories.name #{LIKE} :search AND categories.id = combos.category_id)",
+                                { :search => "#{params[:q].strip}%" })
+                          .limit(params[:limit] || 100)
+        @collection = @collection.order("created_at desc")
+      else
+        @search = @collection.ransack(params[:q])
+        @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
+        @collection = @collection.order("created_at desc") # Intente ordenarlo con ransack pero no pude asi que hago esto 
+      end
+
+#      params[:q] ||= {}
+#      params[:q][:deleted_at_null] ||= "1"
+
+#      params[:q][:s] ||= "name asc"
+#      @collection = Combo.where(nil)
       # Don't delete params[:q][:deleted_at_null] here because it is used in view to check the
       # checkbox for 'q[deleted_at_null]'. This also messed with pagination when deleted_at_null is checked.
       # if params[:q][:deleted_at_null] == '0'
@@ -32,11 +49,11 @@ module Spree::Admin
       # @search needs to be defined as this is passed to search_form_for
       # Temporarily remove params[:q][:deleted_at_null] from params[:q] to ransack products.
       # This is to include all products and not just deleted products.
-      @search = @collection.ransack(params[:q].reject { |k, _v| k.to_s == 'deleted_at_null' })
-      @collection = @search.result.
-            page(params[:page]).
-            per(params[:per_page] || Spree::Config[:admin_products_per_page])
-      @collection
+#      @search = @collection.ransack(params[:q].reject { |k, _v| k.to_s == 'deleted_at_null' })
+#      @collection = @search.result.
+#            page(params[:page]).
+#            per(params[:per_page] || Spree::Config[:admin_products_per_page])
+#      @collection
     end
   end
 end
