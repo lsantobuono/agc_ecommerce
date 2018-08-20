2d636d6e8279e3ec2f3c53860d8b9d375f004d65
Martín <mrosso10@gmail.com>
2018-08-20 14:54:42 -0300

    Quito codigo comentado y index de taxons que no iba

--- app/controllers/spree/admin/complements_controller.rb
+++ app/controllers/spree/admin/complements_controller.rb
@@ -23,25 +23,6 @@ module Spree::Admin
         @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
         @collection = @collection.order("created_at desc") # Intente ordenarlo con ransack pero no pude asi que hago esto 
       end
-
-#      params[:q] ||= {}
-#      params[:q][:deleted_at_null] ||= "1"
-
-#      params[:q][:s] ||= "name asc"
-#      @collection = Complement.where(nil)
-      # Don't delete params[:q][:deleted_at_null] here because it is used in view to check the
-      # checkbox for 'q[deleted_at_null]'. This also messed with pagination when deleted_at_null is checked.
-      # if params[:q][:deleted_at_null] == '0'
-      #   @collection = @collection.with_deleted
-      # end
-      # @search needs to be defined as this is passed to search_form_for
-      # Temporarily remove params[:q][:deleted_at_null] from params[:q] to ransack products.
-      # This is to include all products and not just deleted products.
-#      @search = @collection.ransack(params[:q].reject { |k, _v| k.to_s == 'deleted_at_null' })
-#      @collection = @search.result.
-#            page(params[:page]).
-#            per(params[:per_page] || Spree::Config[:admin_products_per_page])
-#      @collection
     end
   end
 end
