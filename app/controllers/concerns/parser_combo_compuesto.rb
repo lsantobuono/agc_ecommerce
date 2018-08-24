ed473e4b31aaffcf0c442ef1e0aefffcb5155d16
lsantobuono <luciano.santobuono@gmail.com>
2018-08-24 14:20:35 -0300

    Arreglo la funcion para que parsee las cantidades, y los nombres de combos. Y arreglo los acordeones porque duplicaban id

--- app/controllers/concerns/parser_combo_compuesto.rb
+++ app/controllers/concerns/parser_combo_compuesto.rb
@@ -3,11 +3,25 @@ module ParserComboCompuesto
   def parsear_combo_compuestos(string)
     ret = []
     string.downcase.split("-").each do |c|
-      combo = Combo.where('lower(code) = ?', c).first
+      cantidad_combo = 1
+      string_combo = c
+
+      #si el primer char es un integer, significa que viene con cantidad
+      first_char = c[0]
+
+      if first_char.match(/\A-?\d+\Z/).present?
+        cantidad_combo = c[/\d+/]
+        string_combo = c.sub(cantidad_combo,'')
+      end
+
+      combo = Combo.where('lower(code) = ?', string_combo).first
       if (combo.nil?)
         return false,"El id de combo '#{c}' es inválido, por favor chequee que sea correcto, o comuniquese con nosotros."
       else
-        ret.push(combo)
+        i = 1
+        for i in 1..cantidad_combo.to_i
+          ret.push(combo)
+        end
       end
     end
     return true,ret
