c2f6cc508399b55685f22245d7afe07ccc182727
lsantobuono <luciano.santobuono@gmail.com>
2019-02-27 13:58:09 -0300

    Se agregan los precios de los comboos al import

--- app/controllers/spree/admin/price_file_controller.rb
+++ app/controllers/spree/admin/price_file_controller.rb
@@ -14,13 +14,14 @@ module Spree::Admin
 
     def create
       @listaCambios = {}
+      @listaCambiosCombos = {}
       
       uploaded = params[:attachment]
       file = File.open("/tmp/csvprice.xls", 'wb')
       file.write(uploaded.read)
       
       book = Spreadsheet.open(file.path)
-      sheet1 = book.worksheet 1
+      sheet1 = book.worksheet 'ÍNDICE PARA PÁGINA WEB'
       sheet1.each do |row| 
         if (row[0] != nil &&  row[0] != "" &&  row[4] != nil && row[4].value != nil && row[4].value != "" && (is_number? row[4].value))
           var = Spree::Variant.find_by(sku: row[0])
@@ -32,16 +33,37 @@ module Spree::Admin
           end
         end
       end
+      
+      sheet_combos = book.worksheet 'COMBOS'
+      sheet_combos.each do |row|
+        if (row[0].present? && (is_number? row[1]) && (is_number? row[2]) )
+          combo = Combo.find_by(code: row[0])
+          if combo.present?
+            if (
+              (combo.price_cash.nil? || combo.price_mercado_pago.nil?) ||
+              (((combo.price_cash - row[1]).abs > 0.0045) || 
+              ((combo.price_mercado_pago - row[2]).abs > 0.0045)))
+              arrPrice=[]
+              arrPrice[0] = combo.price_cash
+              arrPrice[1] = row[1]
+              arrPrice[2] = combo.price_mercado_pago
+              arrPrice[3] = row[2]
 
+              @listaCambiosCombos[row[0]] = arrPrice
+            end
+          end
+        end
+      end
       render "spree/admin/price_file/index"
     end
 
     def confirm
       @preciosActualizados = {}
+      @preciosActualizadosCombos = {}
 
       file = File.open("/tmp/csvprice.xls", 'rb')
       book = Spreadsheet.open(file.path)
-      sheet1 = book.worksheet 1
+      sheet1 = book.worksheet 'ÍNDICE PARA PÁGINA WEB'
       sheet1.each do |row| 
         if (row[0] != nil &&  row[0] != "" &&  row[4] != nil && row[4].value != nil && row[4].value != "" && (is_number? row[4].value))
           var = Spree::Variant.find_by(sku: row[0])
@@ -57,8 +79,32 @@ module Spree::Admin
           end
         end     
       end
-    render "spree/admin/price_file/index"
+      sheet_combos = book.worksheet 'COMBOS'
+      sheet_combos.each do |row|
+        if (row[0].present? && (is_number? row[1]) && (is_number? row[2]) )
+          combo = Combo.find_by(code: row[0])
+          if combo.present?
+            if (
+              (combo.price_cash.nil? || combo.price_mercado_pago.nil?) ||
+              (((combo.price_cash - row[1]).abs > 0.0045) || 
+              ((combo.price_mercado_pago - row[2]).abs > 0.0045)))
+              auxPriceCash = combo.price_cash
+              auxPriceMercadoPago = combo.price_mercado_pago
+              combo.price_cash = row[1]
+              combo.price_mercado_pago = row[2]
+              if combo.save!
+                arrPrice=[]
+                arrPrice[0] = auxPriceCash
+                arrPrice[1] = row[1]
+                arrPrice[2] = auxPriceMercadoPago
+                arrPrice[3] = row[2]
+                @preciosActualizadosCombos[row[0]] = arrPrice
+              end
+            end
+          end
+        end
+      end
+      render "spree/admin/price_file/index"
     end 
-  
   end
 end
