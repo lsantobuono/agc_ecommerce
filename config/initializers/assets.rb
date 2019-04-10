b971393d806d15c6fb8a89281d27776a857a307a
lsantobuono <luciano.santobuono@gmail.com>
2019-04-10 17:23:39 -0300

    Logger

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( checkout_steps.scss )
+
+Rails.application.config.assets.logger = Logger.new($stdout)
