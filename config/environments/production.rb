5fa9115d163f30f391dda06f1c74e0b71fc67208
lsantobuono <luciano.santobuono@gmail.com>
2019-04-10 19:00:06 -0300

    Intento logger de nuevo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.logger = Logger.new $stdout
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
