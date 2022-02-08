5ffd503c6d7fe359b89ec71c4e26dcc6b4614a74
Martín Rosso <mrosso10@gmail.com>
2022-02-08 11:29:30 -0300

    restore postgres

--- restore_postgres.sh
+++ restore_postgres.sh
@@ -2,14 +2,14 @@
 
 # $1 :   Url de backup
 
-bundle exec rake db:drop
-bundle exec rake db:create
+bundle _1.17.3_ exec rake db:drop
+bundle _1.17.3_ exec rake db:create
 mkdir tmp/bkp
 cd tmp/bkp/
 
 wget $1
-tar -xf agc_ecommerce.tar
-cd agc_ecommerce/databases/
+tar -xf agc.tar
+cd agc/databases/
 gunzip PostgreSQL.sql.gz
 psql -a -d agc_development -f PostgreSQL.sql -U agc
 psql -a -d agc_development -U agc -c 'CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;'
