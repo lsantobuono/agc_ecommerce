673a34500c7d02adeb474cf80cbd46a5398db2e8
Martín Rosso <mrosso10@gmail.com>
2022-02-08 11:32:06 -0300

    Muestro eventualiedades en index de combos

--- app/controllers/spree/combos_controller.rb
+++ app/controllers/spree/combos_controller.rb
@@ -4,6 +4,13 @@ module Spree
 
     def index
       @combos=Combo.all
+      if (Spree::Store.first.eventuality_id !=nil && Spree::Store.first.eventuality_id != 0 )
+        e = Eventuality.find(Spree::Store.first.eventuality_id)
+        if (e != nil)
+          type = (e.type_eventuality == 0) ? :danger : :info 
+          flash.now[type]= e.message
+        end
+      end
     end
 
     def mercado_libre
