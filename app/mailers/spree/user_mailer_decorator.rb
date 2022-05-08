e3cc1f22868cad62043bc4232a5b625e7c65a69f
Martín Rosso <mrosso10@gmail.com>
2022-05-08 14:02:42 -0300

    Fixes

--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -3,7 +3,7 @@ module Spree
 
     def registration_notice(user)
       @name=user.first_name
-      mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' - ' + I18n.t(:subject, :scope => [:devise, :mailer, :registration_notice])
+      mail to: user.email, from: from_address, subject: (Spree::Store.current.name || 'agc') + ' - ' + I18n.t(:subject, :scope => [:devise, :mailer, :registration_notice])
     end
 
   end
